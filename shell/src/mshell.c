9c0446e4d31557e2802c32265d8f5a02fee3cfdb
Alex <alex.domkratov@gmail.com>
2023-10-19 17:34:13 +0200

    refactor: small changes and performance improvements
    - Replace write() with dprintf().
    - Refactor exec_command() to use statically allocated buffer for exec() arguments.
    - Remove arg_count() and vectorize() functions.

--- shell/src/mshell.c
+++ shell/src/mshell.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <malloc.h>
 #include <errno.h>
 
 #include <unistd.h>
@@ -11,9 +10,7 @@
 #include "siparse.h"
 #include "utils.h"
 
-size_t arg_count(argseq* arg_seq);
-
-void vectorize(argseq* arg_seq, char* argv[]);
+#define MAX_ARGS (MAX_LINE_LENGTH / 2)
 
 void exec_command(command* cmd);
 
@@ -26,7 +23,7 @@ int main(int argc, char* argv[])
 
     while (1) {
 
-        (void) write(STDOUT_FILENO, PROMPT_STR, strlen(PROMPT_STR));
+        (void) dprintf(STDOUT_FILENO, PROMPT_STR); // flush()?
 
         ssize_t read_count = read(STDIN_FILENO, line_buf, MAX_LINE_LENGTH);
         if (read_count > 0) {
@@ -35,8 +32,7 @@ int main(int argc, char* argv[])
 
             line = parseline(line_buf);
             if (line == NULL) {
-                char* error_str = SYNTAX_ERROR_STR "\n";
-                (void) write(STDERR_FILENO, error_str, strlen(error_str));
+                (void) dprintf(STDERR_FILENO, SYNTAX_ERROR_STR "\n");
             } else {
                 cmd = pickfirstcommand(line);
                 if (cmd != NULL) {
@@ -45,29 +41,22 @@ int main(int argc, char* argv[])
                         waitpid(pid, NULL, 0);
                     } else if (pid == 0) {
 
-                        exec_command(cmd);
+                        exec_command(cmd); // does not return on success
 
-                        char* msg_fmt;
+                        char* errmsg_fmt;
                         switch (errno) {
                             case ENOENT:
-                                msg_fmt = "%s: no such file or directory\n";
+                                errmsg_fmt = "%s: no such file or directory\n";
                                 break;
                             case EACCES:
-                                msg_fmt = "%s: permission denied\n";
+                                errmsg_fmt = "%s: permission denied\n";
                                 break;
                             default:
-                                printf("%d\n", errno);
-                                msg_fmt = "%s: exec error\n";
+                                errmsg_fmt = "%s: exec error\n";
                         }
 
-                        // is there a more elegant way to do this?
+                        (void) dprintf(STDERR_FILENO, errmsg_fmt, cmd->args->arg);
 
-                        char* exec_name = cmd->args->arg;
-                        char* err_msg = malloc(strlen(exec_name) + strlen(msg_fmt) - 1);
-                        int msg_sz = sprintf(err_msg, msg_fmt, exec_name);
-
-                        (void) write(STDERR_FILENO, err_msg, msg_sz);
-                        free(err_msg);
                         exit(EXEC_FAILURE);
 
                     } // else fork() error
@@ -84,40 +73,19 @@ int main(int argc, char* argv[])
 
 }
 
-size_t arg_count(argseq* arg_seq)
-{
-    size_t count = 0;
-    argseq* arg_iter = arg_seq;
-    do {
-        ++count;
-        arg_iter = arg_iter->next;
-    } while (arg_iter != arg_seq);
-
-    return count;
-
-}
-
-void vectorize(argseq* arg_seq, char** argv)
-{
-    argseq* arg_iter = arg_seq;
-    do {
-        *argv++ = arg_iter->arg;
-        arg_iter = arg_iter->next;
-    } while (arg_iter != arg_seq);
-
-    *argv = NULL;
-
-}
-
 void exec_command(command* cmd)
 {
-    size_t argc = arg_count(cmd->args);
+    char* argv[MAX_ARGS];
+    int argc = 0;
 
-    /* Successful exec() call will reclaim this memory */
-    char** argv = malloc((argc + 1) * sizeof(char*));
-    vectorize(cmd->args, argv);
+    argseq* arg_iter = cmd->args;
+    do {
+        argv[argc++] = arg_iter->arg;
+        arg_iter = arg_iter->next;
+    } while (arg_iter != cmd->args);
+
+    argv[argc] = NULL;
 
     (void) execvp(argv[0], argv);
-    free(argv);
 
 }
