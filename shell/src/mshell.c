7942f5f35644b1763fa2e3de0eb5d0a7d1ed0f35
Alex <alex.domkratov@gmail.com>
2023-11-10 23:27:54 +0100

    feature: implementation of stage 4
    Work In Progress

--- shell/src/mshell.c
+++ shell/src/mshell.c
@@ -5,6 +5,7 @@
 
 #include <unistd.h>
 #include <sys/wait.h>
+#include <fcntl.h>
 
 #include "config.h"
 #include "siparse.h"
@@ -14,8 +15,9 @@
 
 #define MAX_ARGS (MAX_LINE_LENGTH / 2)
 
-int exec_builtin(command* cmd, builtin_fun_t fun);
-int exec_command(command* cmd);
+void exec_builtin(command* cmd, builtin_fun_t fun);
+
+void exec_command(command* cmd);
 
 int main(int argc, char* argv[])
 {
@@ -35,47 +37,32 @@ int main(int argc, char* argv[])
             (void) dprintf(STDERR_FILENO, SYNTAX_ERROR_STR "\n");
         } else {
 
-            command* cmd = pickfirstcommand(seq);
-            if (cmd == NULL) continue;
+            pipelineseq* iter = seq;
+            do {
 
-            builtin_fun_t fun = find_builtin_fun(cmd->args->arg);
-            if (fun != NULL) {
+                command* cmd = pickfirstcommand(iter);
+                if (cmd == NULL) continue;
 
-                int code = exec_builtin(cmd, fun);
-                if (code != 0) {
-                    (void) dprintf(STDERR_FILENO, "Builtin %s error.\n", cmd->args->arg);
-                }
+                builtin_fun_t fun = find_builtin_fun(cmd->args->arg);
+                if (fun != NULL) {
 
-            } else {
+                    exec_builtin(cmd, fun);
 
-                pid_t pid = fork();
-                if (pid > 0) {
-                    waitpid(pid, NULL, 0);
-                } else if (pid == 0) {
+                } else {
 
-                    (void) exec_command(cmd); // does not return on success
-
-                    char* errmsg_fmt;
-                    switch (errno) {
-                        case ENOENT:
-                            errmsg_fmt = "%s: no such file or directory\n";
-                            break;
-                        case EACCES:
-                            errmsg_fmt = "%s: permission denied\n";
-                            break;
-                        default:
-                            errmsg_fmt = "%s: exec error\n";
+                    pid_t pid = fork();
+                    if (pid > 0) {
+                        waitpid(pid, NULL, 0);
+                    } else if (pid == 0) {
+                        exec_command(cmd); // does not return on success
+                        exit(EXIT_FAILURE);
+                    } else { // fork() error
+                        break;
                     }
 
-                    (void) dprintf(STDERR_FILENO, errmsg_fmt, cmd->args->arg);
-
-                    exit(EXEC_FAILURE);
-
-                } else { // fork() error
-                    break;
                 }
 
-            }
+            } while ((iter = iter->next) != seq);
 
         }
 
@@ -99,18 +86,88 @@ void vectorize(command* cmd, char* argv[])
 
 }
 
-int exec_builtin(command* cmd, builtin_fun_t fun)
+void exec_builtin(command* cmd, builtin_fun_t fun)
 {
     char* argv[MAX_ARGS];
     vectorize(cmd, argv);
 
-    return fun(argv);
+    if (fun(argv) != 0) {
+        (void) dprintf(STDERR_FILENO, "Builtin %s error.\n", cmd->args->arg);
+    }
 }
 
-int exec_command(command* cmd)
+int redirect(redir* r);
+
+void exec_command(command* cmd)
 {
+    if (cmd->redirs != NULL) {
+        redirseq* seq = cmd->redirs;
+        do {
+            if (redirect(seq->r) == -1) return;
+            seq = seq->next;
+        } while (seq != cmd->redirs);
+    }
+
     char* argv[MAX_ARGS];
     vectorize(cmd, argv);
 
-    return execvp(argv[0], argv);
+    (void) execvp(argv[0], argv);
+
+    char* errmsg_fmt;
+    switch (errno) {
+        case ENOENT:
+            errmsg_fmt = "%s: no such file or directory\n";
+            break;
+        case EACCES:
+            errmsg_fmt = "%s: permission denied\n";
+            break;
+        default:
+            errmsg_fmt = "%s: exec error\n";
+    }
+
+    (void) dprintf(STDERR_FILENO, errmsg_fmt, cmd->args->arg);
+
+}
+
+int redirect(redir* r)
+{
+    int fd = -1;
+    if (IS_RIN(r->flags)) {
+        fd = open(r->filename, O_RDONLY);
+    } else if (IS_ROUT(r->flags)) {
+        fd = open(r->filename, O_WRONLY | O_CREAT | O_TRUNC,
+                  S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+    } else if (IS_RAPPEND(r->flags)) {
+        fd = open(r->filename, O_WRONLY | O_CREAT | O_APPEND,
+                  S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+    } // else error?
+
+    if (fd != -1) {
+
+        if (IS_RIN(r->flags)) {
+            dup2(fd, STDIN_FILENO);
+            close(fd);
+        } else {
+            dup2(fd, STDOUT_FILENO);
+            close(fd);
+        }
+        return fd;
+
+    }
+
+    char* errmsg_fmt;
+    switch (errno) {
+        case ENOENT:
+            errmsg_fmt = "%s: no such file or directory\n";
+            break;
+        case EACCES:
+            errmsg_fmt = "%s: permission denied\n";
+            break;
+        default:
+            errmsg_fmt = "%s: redirection error\n";
+    }
+
+    (void) dprintf(STDERR_FILENO, errmsg_fmt, r->filename);
+    return -1;
+
 }
